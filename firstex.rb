7497284e9971fb711f083f70ccdbf57dec351889
Thiago <thhsmonteiro@gmail.com>
2024-10-02 20:07:47 -0300

    Commiting an exercise for a course

---
+++ firstex.rb
@@ -0,0 +1,9 @@
+puts "----------library----------"
+
+puts "Enter your fullname: "
+fullname = gets.chomp
+
+puts "Enter your age: "
+age = gets.chomp.to_i
+
+puts "Your fullname is #{fullname} and you are #{age} years old"
