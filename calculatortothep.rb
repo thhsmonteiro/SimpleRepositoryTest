bf93759dc04cb5c92eb6a73cab49eb2309d37a08
Thiago <thhsmonteiro@gmail.com>
2024-10-03 20:29:51 -0300

    Adding the calculator of the power of three to the dio's project

---
+++ calculatortothep.rb
@@ -0,0 +1,13 @@
+
+puts "Add the first number to the array: "
+f1 = gets.chomp.to_i
+
+puts "Add the second number to the array: "
+f2 = gets.chomp.to_i
+
+puts "Add the third number to the array: "
+f3 = gets.chomp.to_i
+
+array = [f1**3, f2**3, f3**3]
+
+puts "Thoses numbers to the power of three are: #{array}"
